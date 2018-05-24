4a634a6e8224782d761fa537ae36205a30067269
Guohua Wu <ghmeta@163.com>
2018-05-24 22:56:49 +0800

    add exercise 112.java

---
+++ src/fundamentals/programModel/Exercise112.java
@@ -0,0 +1,9 @@
+package fundamentals.programModel;
+import edu.princeton.cs.algs4.*;
+
+public class Exercise112 {
+    public static void main(String[] args) {
+        System.out.print(1+2+"3");
+        StdOut.println("Please enter three numbers");
+    }
+}
