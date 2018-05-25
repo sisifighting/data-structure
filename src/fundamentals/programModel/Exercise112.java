4ed7cf4cae6ca3d4c2a008c7a3562e960bebb357
zhangsiyu <zhangsiyu@cmhi.chinamobile.com>
2018-05-25 09:34:17 +0800

    change name

--- src/fundamentals/programModel/Exercise112.java
+++
@@ -1,9 +0,0 @@
-package fundamentals.programModel;
-import edu.princeton.cs.algs4.*;
-
-public class Exercise112 {
-    public static void main(String[] args) {
-        System.out.print(1+2+"3");
-        StdOut.println("Please enter three numbers");
-    }
-}
