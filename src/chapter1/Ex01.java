1ebaef432bf2c2385ddd4e8e72538cd8ee59b3a7
zhangsiyu <zhangsiyu@cmhi.chinamobile.com>
2018-05-25 10:58:36 +0800

    test

--- src/chapter1/Ex01.java
+++ src/chapter1/Ex01.java
@@ -1,7 +1,8 @@
+import edu.princeton.cs.algs4.*;
 public class Ex01 {
  public static void main(String[] args) {
-  System.out.println((0 + 15) / 2);
-  System.out.println(2.0e-6 * 100000000.1);
-  System.out.println(true && false || true && true);
+  StdOut.println((0 + 15) / 2);
+  StdOut.println(2.0e-6 * 100000000.1);
+  StdOut.println(true && false || true && true);
  }
 }
